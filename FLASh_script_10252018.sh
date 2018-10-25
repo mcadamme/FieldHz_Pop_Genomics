b3c77eac39321aa3b73aaf4be489f395c923d740
mcadamme <mcadamme@msu.edu>
2018-10-25 13:15:41 -0400

    completed checked and run script to merge R1 and R2 with FLASh

--- FLASh_script_10252018.sh
+++ FLASh_script_10252018.sh
@@ -20,6 +20,7 @@ mkdir FLASH_output_AMD_lib8
 mv AMD_lib8* ./FLASH_output_AMD_lib8
 
 ###New File AMD lib7
+###Lib7 pool 4 was also low yielding
 
 cd ../AMD_lib7_raw
 mkdir FLASH_output_AMD_lib7
@@ -43,7 +44,7 @@ mkdir FLASH_output_AMD_lib6
 
 ~/src/FLASH-1.2.11-Linux-x86_64/flash AMD-6-1_S1_L001_R1_001.fastq.gz AMD-6-1_S1_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib6_pool1 >&1 | tee AMD_lib6_pool1_flash.log
 
-~/src/FLASH-1.2.11-Linux-x86_64/flash AMD-6-2_S2_L001_R1_001.fastq.gz AMD-6-2_S2_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib6_pool1 >&1 | tee AMD_lib6_pool2_flash.log
+~/src/FLASH-1.2.11-Linux-x86_64/flash AMD-6-2_S2_L001_R1_001.fastq.gz AMD-6-2_S2_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib6_pool2 >&1 | tee AMD_lib6_pool2_flash.log
 
 ~/src/FLASH-1.2.11-Linux-x86_64/flash AMD-6-3_S3_L001_R1_001.fastq.gz AMD-6-3_S3_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib6_pool3 >&1 | tee AMD_lib6_pool3_flash.log
 
@@ -55,6 +56,7 @@ mv AMD_lib6* ./FLASH_output_AMD_lib6
 
 
 ###New file AMD_lib5
+#note Lib5 index2 had low read counts
 
 cd ../AMD_lib5_raw
 mkdir FLASH_output_AMD_lib5
@@ -127,8 +129,10 @@ mkdir FLASH_output_AMD_lib1
 
 ~/src/FLASH-1.2.11-Linux-x86_64/flash index4_S3_L001_R1_001.fastq.gz index4_S3_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib1_idx4 >&1 | tee AMD_lib1_idx4_flash.log
 
-~/src/FLASH-1.2.11-Linux-x86_64/flash index6_S4_L001_R1_001.fastq.gz index6_S4_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib1_idx6 >&1 | tee AMD_lib1_idx6_flash.log
+~/src/FLASH-1.2.11-Linux-x86_64/flash index12_S4_L001_R1_001.fastq.gz index12_S4_L001_R2_001.fastq.gz --max-overlap=200 -o AMD_lib1_idx12 >&1 | tee AMD_lib1_idx12_flash.log
 
 mv AMD_lib1* ./FLASH_output_AMD_lib1
-###Copying all Flash output folders to new directory for demultiplexing.
+
+
+###Copied all Flash output folders to new directory (using gui) for demultiplexing.
 
