67f3cbf881ccee0e8bb7b2a91f32091229b81c7b
mcadamme <mcadamme@msu.edu>
2018-11-08 14:09:33 -0500

    switched from using samtools mpileup to using bcftools v. 1.9 with mpileup and view modules.

--- SNP_calling_with_mpileup.sh
+++ SNP_calling_with_mpileup.sh
@@ -1,6 +1,5 @@
 #script for SNP calling
 #MF 11/1/2018
-#samtools v. 1.9
 #bcftools v. 1.9
 
 
@@ -8,9 +7,7 @@ cd /media/megan/"New Volume"/Hz_PopGen_ddRAD_demult/Bowtie_genome_alignments
 
 mkdir mpileupANDvcftools_output
 
-samtools mpileup -uf ./Hzea_genome/GCA_002150865.1_Hzea_1.0_genomic.fna -b ./Hzea_BamFiles.txt -d 70 > FieldHzea.bcf  
-
-bcftools call -vmO v -o ./mpileupANDvcftools_output/FieldHzea_calling.bcf ./mpileupANDvcftools_output/FieldHzea.bcf  
+bcftools mpileup -f ./Hzea_genome/GCA_002150865.1_Hzea_1.0_genomic.fna -b ./Hzea_BamFiles.txt -I -d 70 --threads 6 -O u -o ./mpileupANDvcftools_output/FieldHzea.bcf
 
 bcftools view ./samtoolsANDvcftools_output/FieldHzea_calling.bcf > ./samtoolsANDvcftools_output/FieldHzea.vcf #conversion to vcf format
 
