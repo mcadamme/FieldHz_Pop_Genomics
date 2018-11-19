293119aa74b7b8c9b9a8901b8499e5461046bce8
mcadamme <mcadamme@msu.edu>
2018-11-19 12:20:17 -0500

    replotted read count data and relaxed filtering criteria for removal of individuals due to low read counts

--- plot_HzField_read_counts.R
+++ plot_HzField_read_counts.R
@@ -22,24 +22,24 @@ data$samp_num <- (1:nrow(data))
 mean_read_count <- mean(data$read_counts)
 sd_read_count <- sd(data$read_counts)
 sum_read_count <- sum(data$read_counts)
-y.low <- rep(mean_read_count*0.1, times = nrow(data))
+y.low <- rep(mean_read_count*0.01, times = nrow(data))
 y.high <- rep(max(data$read_counts), times = nrow(data))
 
-exclude_from_analysis <- subset(data, read_counts < (mean_read_count * 0.1))
-exclude_from_analysis #all have fewer than 40,000 reads 
+exclude_from_analysis <- subset(data, read_counts < (mean_read_count*0.01))
+exclude_from_analysis #all have fewer than 4,000 reads 
 #these individuals should be excluded from pop-level analysis with vcftools.
 
 col_year = c("red","green","blue","black" )[as.factor(data$year)] 
          
 png("Hz_fieldcoll_readcounts.png", units = "px", width = 600, height = 400)
-plot(data$read_counts~data$samp_num, col = col_year, pch = 16, xaxt = "n", 
-     ylab = "Read Counts", xlab = "Samples Grouped By Year", cex.lab = 1.25, cex.axis = 1.2)
-lines(data$samp_num, y.low, col = 'grey')
-lines(data$samp_num, y.high, col = 'grey')
+plot(log10(data$read_counts)~data$samp_num, col = col_year, pch = 16, xaxt = "n", 
+     ylab = "Log10 Read Counts", xlab = "Samples Grouped By Year", ylim = c(2,8), cex.lab = 1.25, cex.axis = 1.2)
+lines(data$samp_num, log10(y.low), col = 'grey')
+lines(data$samp_num, log10(y.high), col = 'grey')
 
-polygon(c(data$samp_num, rev(data$samp_num)), c(y.high, rev(y.low)),
+polygon(c(data$samp_num, rev(data$samp_num)), c((log10(y.high)), rev(log10(y.low))),
         col=rgb(0.6,0.6,0.7,0.2), border = NA)
-abline(a = mean_read_count, b = 0)
+abline(a = log10(mean_read_count), b = 0)
 
 dev.off()
 
