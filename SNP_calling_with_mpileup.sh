dec60f2f19298e67981587a97b8c1d01fb646c8f
mcadamme <mcadamme@msu.edu>
2018-11-19 12:25:41 -0500

    changed the SNP calling software from samtools to bcftools only, using the bcftools mpileup and call functions.

--- SNP_calling_with_mpileup.sh
+++ SNP_calling_with_mpileup.sh
@@ -9,6 +9,11 @@ mkdir mpileupANDvcftools_output
 
 bcftools mpileup -f ./Hzea_genome/GCA_002150865.1_Hzea_1.0_genomic.fna -b ./Hzea_BamFiles.txt -I -d 70 --threads 6 -O u -o ./mpileupANDvcftools_output/FieldHzea.bcf
 
-bcftools view ./samtoolsANDvcftools_output/FieldHzea_calling.bcf > ./samtoolsANDvcftools_output/FieldHzea.vcf #conversion to vcf format
+bcftools call -vmO v -o ./mpileupANDvcftools_output/FieldHzea_variantsonly.vcf ./mpileupANDvcftools_output/FieldHzea.bcf #SNP calling and conversion to vcf formatcf
+
+#here I filtered my called SNPs in preparation for analysis in R
+
+vcftools --vcf FieldHzea_variantsonly.vcf --recode --out thinned_FieldHzea_variantsonly.vcf --minDP 3  --min-alleles 2 --max-alleles 2 --maf 0.05 --max-missing 0.75
+ 
 
 
