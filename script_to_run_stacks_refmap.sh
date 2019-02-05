76b351343ee86548c3503fedd6026f11d805885a
mcadamme <mcadamme@msu.edu>
2019-02-05 10:46:24 -0500

    removed populations filtering and instead used vcftools for filtering using same parameters as those used for filtering BCFtools dataset

--- script_to_run_stacks_refmap.sh
+++ script_to_run_stacks_refmap.sh
@@ -4,8 +4,10 @@
 
 cd /media/megan/"New Volume"/Hz_PopGen_ddRAD_demult/Bowtie_genome_alignments
 
-mkdir ./stacks_output
+#mkdir ./stacks_output
 
-ref_map.pl -T 6 --samples ./ --popmap /home/megan/scripts/Field_HZ_Pop_Genomics/pop_files/all_pops_no_reps_for_Stacks.txt -o ./stacks_output -X populations:"-r 0.75 -min_maf 0.05 --vcf --fasta_samples --vcf_haplotypes"
+ref_map.pl -T 6 --samples ./ --popmap /home/megan/scripts/Field_HZ_Pop_Genomics/pop_files/all_pops_no_reps_for_Stacks.txt -o ./stacks_output -X "gstacks: --min-mapq 5" -X "populations: --vcf --fasta_samples --vcf_haplotypes"
  
-vcftools --vcf ./stacks_output/XXX.vcf --recode --out ./stacks_output/thinned_FieldHzea_variantsonly_stacks.vcf --minDP 3  --min-alleles 2 --max-alleles 2
+vcftools --vcf ./stacks_output/populations.snps.vcf --recode --out ./stacks_output/thinned_FieldHzea_variantsonly_stacks.vcf --minDP 3  --min-alleles 2 --max-alleles 2 --maf 0.05 --max-missing 0.75 --keep /home/megan/scripts/Field_HZ_Pop_Genomics/pop_files/allpops_no_low_stacks.txt
+
+#Rerunning populations to get haplotype information.
