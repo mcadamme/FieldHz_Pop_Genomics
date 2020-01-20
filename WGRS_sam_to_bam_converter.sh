1f6d44740b5add0e5365cd089c7b89e71fa7e15c
mcadamme <mcadamme@msu.edu>
2020-01-20 06:50:39 -0500

    added sanity check to make sure gatk removed dups

--- WGRS_sam_to_bam_converter.sh
+++ WGRS_sam_to_bam_converter.sh
@@ -4,7 +4,7 @@
 
 #used samtools 0.1.19 
 
-cd /media/megan/"New Volume"/Hzea_WGRS_Bowtie2_output
+cd /media/megan/"New Volume"/Hzea_WGRS_Bowtie2_output/Alignment_by_years_01142020
 
 for sample in *.sam
 do
@@ -27,51 +27,55 @@ do
 done
 
 
-#filtering bam files based on mapping quality (applies to v.2 files)
+#filtering bam files based on mapping quality
 
-for sample in *_paired.bam
+for sample in *.bam
 do
 	echo $sample
-	describer=$(echo ${sample} | sed 's/_paired.bam//')
+	describer=$(echo ${sample} | sed 's/.bam//')
 	echo $describer
 	
 	#filtering based on quality score
 	samtools view -bq 5 $sample -o ${describer}.uns.bam
 
 	#Sort BAM file
-	samtools sort ${describer}.uns.bam ${describer}_filtered.paired
+	samtools sort ${describer}.uns.bam ${describer}_filtered
 
 	#Index BAM file
-	samtools index ${describer}_filtered.paired.bam
+	samtools index ${describer}_filtered.bam
 
 	#Remove intermediate files
 	rm ${describer}.uns.bam
 
 done
 
-#marking and removing duplicates (applies to v.2 files)
-for sample in *_filtered.paired.bam
+#marking and removing duplicates
+for sample in *_filtered.bam
 do
 	echo $sample
-	describer=$(echo ${sample} | sed 's/_filtered.paired.bam//')
+	describer=$(echo ${sample} | sed 's/_filtered.bam//')
 	echo $describer
 	
 	#convert file from SAM to BAM format
-	/home/megan/src/gatk-4.0.12.0/gatk MarkDuplicates -I $sample -M ${describer}.metrics --REMOVE_DUPLICATES true -O ${describer}.marked.uns.bam
+	/home/megan/src/gatk-4.0.12.0/gatk MarkDuplicates -I $sample -M ${describer}.metrics --REMOVE_DUPLICATES true -O ${describer}_marked.uns.bam
 
 	#Sort filtered BAM file
-	samtools sort ${describer}.marked.uns.bam ${describer}.marked
+	samtools sort ${describer}_marked.uns.bam ${describer}_marked
 
 	#Index BAM file
-	samtools index ${describer}.marked.bam
+	samtools index ${describer}_marked.bam
 
 	#Remove intermediate files
-	rm ${describer}.marked.uns.bam
+	rm ${describer}_marked.uns.bam
 
 done
+
+#sanity check - no dups left
+/home/megan/src/gatk-4.0.12.0/gatk MarkDuplicates -I HZ_2002_01_marked.bam -M HZ_2002_01_marked.metrics --REMOVE_DUPLICATES false -O HZ_2002_01_marked.uns.bam
+rm HZ_2002_01_marked.uns.bam
+
 
 #getting a list of bam files
 ls *marked.bam > WGRS_Hzea_BamFiles.txt
 
-#getting primary aligned reads for each file.
-samtools view -c -F 260 *marked.bam
+
