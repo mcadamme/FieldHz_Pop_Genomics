0473c4cff8ce09946cceca726a77ee5fe8bcaefa
mcadamme <mcadamme@msu.edu>
2020-03-12 15:18:11 -0400

    added probabilities

--- ms_sims_scripts/comp_ms_sim_plots.R
+++ ms_sims_scripts/comp_ms_sim_plots.R
@@ -9,7 +9,7 @@ data_3 <- read.table("~/ms_sims/FieldPops/Ne_tenthou_NoSize/FST_msSims3.out", he
 data_4 <- read.table("~/ms_sims/FieldPops/Ne_twentythou_NoSize/FST_msSims4.out", header = F)
 
 
-df <- rbind(data_4, data_3, data_2, data_1)
+df <- rbind(data_1, data_2, data_3, data_4)
 sim_name <- rep(c("4","3","2","1"), times = 1, each = 10000)
 
 df2 <- cbind(df, sim_name)
@@ -30,7 +30,7 @@ mu
 mu <-unlist(mu)
 sims <- c("4","3","2","1")
 mu2 <- data.frame(cbind(mu, sims))
-mu2$mu <- as.numeric(as.character(mu2$mu))
+mu2$mu <-rev(as.numeric(as.character(mu2$mu)))
 
 
 #comparing all datasets
@@ -42,11 +42,27 @@ a <- ggplot(df3, aes(x = df3$V1, fill = df3$sim_name)) +
   labs(x=expression(F["ST"]*" value"), y=expression("Density")) +
   theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) 
 
-#a + geom_histogram(binwidth = 0.005, alpha = 0.4) +
-  a + geom_density(mapping = aes(color = df3$sim_name), data = df3, stat = "density", alpha = 0.1) +
+  #a + geom_histogram(binwidth = 0.005, alpha = 0.3) +
+  a + geom_density(mapping = aes(color = df3$sim_name), data = df3, stat = "density", alpha = 0.3) +
   scale_color_manual(name = expression("Mean F"["ST"]), labels = c("n=20,000", "n=10,000", "n=5,000", "n=2,500"), values = c("#868686FF","#0073C2FF","#ff1f39","#EFC000FF")) +
   geom_vline(aes(xintercept = mu, color = sims), data = mu2, linetype = "dashed")
 
 dev.off()
 
+#getting p-vals - relative to 6 s.d. threshold
+nrow(subset(data_1, V1 > 0.047))/nrow(na.omit(data_1))#inflated b/c nrow data_1 very low (998)
+nrow(subset(data_2, V1 > 0.047))/nrow(na.omit(data_2))
+nrow(subset(data_3, V1 > 0.047))/nrow(na.omit(data_3))
+nrow(subset(data_4, V1 > 0.047))/nrow(na.omit(data_4))
 
+#using cutoff of FST = 0.146, which is 40kb window max for 2002-2017 comparison @scaf569 containing carboxyQ 
+nrow(subset(data_1, V1 > 0.146))/nrow(na.omit(data_1))#inflated b/c nrow data_1 very low (998)
+nrow(subset(data_2, V1 > 0.146))/nrow(na.omit(data_2))
+nrow(subset(data_3, V1 > 0.146))/nrow(na.omit(data_3))
+nrow(subset(data_4, V1 > 0.146))/nrow(na.omit(data_4))
+
+#using cutoff of FST = 0.204, which is 40kb window max for 2002-2017 comparison @scaf1612 containing cyp333b3
+nrow(subset(data_1, V1 > 0.204))/nrow(na.omit(data_1))#inflated b/c nrow data_1 very low (998)
+nrow(subset(data_2, V1 > 0.204))/nrow(na.omit(data_2))
+nrow(subset(data_3, V1 > 0.204))/nrow(na.omit(data_3))
+nrow(subset(data_4, V1 > 0.204))/nrow(na.omit(data_4))
