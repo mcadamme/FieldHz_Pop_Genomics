f00f97e6fe46ed70de324bc71dd66feaeead3e67
mcadamme <mcadamme@msu.edu>
2021-03-24 10:02:29 -0400

    file reorganization

---
+++ Upstream_Processing_Analysis_ddRAD/file_renamers/AMD_lib3_sample_renamer.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+cd /media/megan/"New Volume"/Hz_PopGen_ddRAD_demult/AMD_libs1thru8_extended_frags
+
+#run AMD_lib1 renamer first
+
+# Index 1
+cp FLASH_output_AMD_lib3/demult3_1/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2002_early_22.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_CTATTA.fq.gz ./renamed_samples/HZ_2002_late_24.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2012_late_38.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2007_early_24.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_GTACTT.fq.gz ./renamed_samples/HZ_2007_late_47.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_GTGAGGGT.fq.gz ./renamed_samples/HZ_2012_early_67.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_TATCGGGA.fq.gz ./renamed_samples/HZ_2002_early_23.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_TTCCTGGA.fq.gz ./renamed_samples/HZ_2002_late_18_rep.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_CGTGTGGT.fq.gz ./renamed_samples/HZ_2007_late_48.fq.gz
+cp FLASH_output_AMD_lib3/demult3_1/sample_GGATTGGT.fq.gz ./renamed_samples/HZ_2007_early_25.fq.gz
+
+# Index 2
+cp FLASH_output_AMD_lib3/demult3_2/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2007_early_26.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_CTATTA.fq.gz ./renamed_samples/HZ_2002_early_24.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2002_late_26.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2007_late_49.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_GTACTT.fq.gz ./renamed_samples/HZ_2012_late_39.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_GTGAGGGT.fq.gz ./renamed_samples/HZ_2012_late_40.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_TATCGGGA.fq.gz ./renamed_samples/HZ_2012_early_68.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_TTCCTGGA.fq.gz ./renamed_samples/HZ_2002_early_25.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_CGTGTGGT.fq.gz ./renamed_samples/HZ_2002_late_23_rep.fq.gz
+cp FLASH_output_AMD_lib3/demult3_2/sample_GGATTGGT.fq.gz ./renamed_samples/HZ_2007_late_50.fq.gz
+
+# Index 4
+cp FLASH_output_AMD_lib3/demult3_4/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2002_late_28.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_CTATTA.fq.gz ./renamed_samples/HZ_2012_late_41.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2007_early_27.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2012_early_69.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_GTACTT.fq.gz ./renamed_samples/HZ_2002_late_29.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_GTGAGGGT.fq.gz ./renamed_samples/HZ_2007_late_51.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_TATCGGGA.fq.gz ./renamed_samples/HZ_2007_early_28.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_TTCCTGGA.fq.gz ./renamed_samples/HZ_2012_late_42.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_CGTGTGGT.fq.gz ./renamed_samples/HZ_2012_early_70.fq.gz
+cp FLASH_output_AMD_lib3/demult3_4/sample_GGATTGGT.fq.gz ./renamed_samples/HZ_2002_early_26.fq.gz
+
+# Index 6
+cp FLASH_output_AMD_lib3/demult3_6/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2012_early_71.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_CTATTA.fq.gz ./renamed_samples/HZ_2007_late_52.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2012_early_72.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2012_late_43.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_GTACTT.fq.gz ./renamed_samples/HZ_2002_early_27.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_GTGAGGGT.fq.gz ./renamed_samples/HZ_2002_early_28.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_TATCGGGA.fq.gz ./renamed_samples/HZ_2002_late_30.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_TTCCTGGA.fq.gz ./renamed_samples/HZ_2007_late_53.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_CGTGTGGT.fq.gz ./renamed_samples/HZ_2007_early_29.fq.gz
+cp FLASH_output_AMD_lib3/demult3_6/sample_GGATTGGT.fq.gz ./renamed_samples/HZ_2012_late_44.fq.gz
+
+
---
+++ Upstream_Processing_Analysis_ddRAD/file_renamers/AMD_lib6_sample_renamer.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+cd /media/megan/"New Volume"/Hz_PopGen_ddRAD_demult/AMD_libs1thru8_extended_frags
+
+#run AMD_lib1 renamer first
+
+#Pool 1 Index 2
+cp FLASH_output_AMD_lib6/demult6_1/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2002_6_1.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_CTATTA.fq.gz ./renamed_samples/HZ_2016_6_2.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2012_6_3.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2016_6_4.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_TGCAAGGA.fq.gz ./renamed_samples/HZ_2007_6_5.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_TGGTACGT.fq.gz ./renamed_samples/HZ_2016_6_6.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_TCTCAGTC.fq.gz ./renamed_samples/HZ_2016_6_7.fq.gz
+cp FLASH_output_AMD_lib6/demult6_1/sample_CCGGATAT.fq.gz ./renamed_samples/HZ_2012_6_8.fq.gz
+
+#Pool 2 Index 1
+cp FLASH_output_AMD_lib6/demult6_2/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2016_6_9.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_CTATTA.fq.gz ./renamed_samples/HZ_2007_6_10.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2002_6_11.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2016_6_12.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_TGCAAGGA.fq.gz ./renamed_samples/HZ_2012_6_13.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_TGGTACGT.fq.gz ./renamed_samples/HZ_2016_6_14.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_TCTCAGTC.fq.gz ./renamed_samples/HZ_2002_6_15.fq.gz
+cp FLASH_output_AMD_lib6/demult6_2/sample_CCGGATAT.fq.gz ./renamed_samples/HZ_2016_6_16.fq.gz
+
+#Pool 3 Index 4
+cp FLASH_output_AMD_lib6/demult6_3/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2012_6_17.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_CTATTA.fq.gz ./renamed_samples/HZ_2016_6_18.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2016_6_19.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2007_6_20.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_TGCAAGGA.fq.gz ./renamed_samples/HZ_2016_6_21.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_TGGTACGT.fq.gz ./renamed_samples/HZ_2002_6_22.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_TCTCAGTC.fq.gz ./renamed_samples/HZ_2012_6_23.fq.gz
+cp FLASH_output_AMD_lib6/demult6_3/sample_CCGGATAT.fq.gz ./renamed_samples/HZ_2016_6_24.fq.gz
+
+#Pool 4 Index 6
+cp FLASH_output_AMD_lib6/demult6_4/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2016_6_25.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_CTATTA.fq.gz ./renamed_samples/HZ_2012_6_26.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2016_6_27.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2016_6_28.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_TGCAAGGA.fq.gz ./renamed_samples/HZ_2002_6_29.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_TGGTACGT.fq.gz ./renamed_samples/HZ_2007_6_30.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_TCTCAGTC.fq.gz ./renamed_samples/HZ_2016_6_31.fq.gz
+cp FLASH_output_AMD_lib6/demult6_4/sample_CCGGATAT.fq.gz ./renamed_samples/HZ_2007_6_32.fq.gz
+
+#Pool 5 Index 10
+cp FLASH_output_AMD_lib6/demult6_5/sample_CGCGGT.fq.gz ./renamed_samples/HZ_2007_6_33.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_CTATTA.fq.gz ./renamed_samples/HZ_2002_6_34.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_GCCAGT.fq.gz ./renamed_samples/HZ_2016_6_35.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_GGAAGA.fq.gz ./renamed_samples/HZ_2012_6_36.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_TGCAAGGA.fq.gz ./renamed_samples/HZ_2016_6_37.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_TGGTACGT.fq.gz ./renamed_samples/HZ_2016_6_38.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_TCTCAGTC.fq.gz ./renamed_samples/HZ_2007_6_39.fq.gz
+cp FLASH_output_AMD_lib6/demult6_5/sample_CCGGATAT.fq.gz ./renamed_samples/HZ_2016_6_40.fq.gz
+
+
