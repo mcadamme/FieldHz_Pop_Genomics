842715a388c97ac696ac5623d5870c6ac20a8925
mcadamme <mcadamme@msu.edu>
2021-04-12 06:37:24 -0400

    used multiple tests for statistical significance

--- QTL/Plot_QTL.R
+++ QTL/Plot_QTL.R
@@ -11,14 +11,14 @@ Full_Ord_Scafs <- read.table(file = "Hzea_superScaf_genome.txt", header = T)
 
 ##### Function to subset Sig QTL at diff thresholds #####
 
-sig_levels <- list(0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001)
+sig_levels <- list(0.01, 0.005, 0.001)
 df <- data.frame()
 
 getSig_QTL <- function(X, outfile){
   for (i in sig_levels){
     print(i)
     X$p_lrt <- as.numeric(as.character(X$p_lrt))
-    temp_df <- subset(X, p_lrt < i)
+    temp_df <- subset(X, p_lrt < i & gamma > 0.01)
     temp_df$sigLev <- rep(i, times = nrow(temp_df))
     print(head(temp_df))
     df <- rbind(df, temp_df)
@@ -29,21 +29,21 @@ write.csv(df, file = outfile)
 
 ##### loading QTL SNPs & removing low freq allele s#####
 
-BAP11A1_DD <- read.csv("BAP11A1_DD_all_snps.csv", header = T)#BC0805
+BAP11A1_DD <- read.csv("BAP11A1_DD_all_snps_4.5.21.csv", header = T)#BC0805
 str(BAP11A1_DD)
-sub_BAP11A1_DD <- subset(BAP11A1_DD, allele_frequency > 0.099)
+sub_BAP11A1_DD <- subset(BAP11A1_DD, allele_frequency > 0.0499)
 
-BAP11A1_CL <- read.csv("BAP11A1_CL_all_snps.csv", header = T)#providence
+BAP11A1_CL <- read.csv("BAP11A1_CL_all_snps_4.5.21.csv", header = T)#providence
 str(BAP11A1_CL)
-sub_BAP11A1_CL <- subset(BAP11A1_CL, allele_frequency > 0.099)
+sub_BAP11A1_CL <- subset(BAP11A1_CL, allele_frequency > 0.0499)
 
-DEO9A1_DD <- read.csv("DEO9A1_DD_all_snps.csv", header = T)#ObsII
+DEO9A1_DD <- read.csv("DEO9A1_DD_all_snps_4.5.21.csv", header = T)#ObsII
 str(DEO9A1_DD)
-sub_DEO9A1_DD <- subset(DEO9A1_DD, allele_frequency > 0.099)
+sub_DEO9A1_DD <- subset(DEO9A1_DD, allele_frequency > 0.0499)
 
-DEO9A1_CL <- read.csv("DEO9A1_CL_all_snps.csv", header = T)#Obs
+DEO9A1_CL <- read.csv("DEO9A1_CL_all_snps_4.5.21.csv", header = T)#Obs
 str(DEO9A1_CL)
-sub_DEO9A1_CL <- subset(DEO9A1_CL, allele_frequency > 0.099)
+sub_DEO9A1_CL <- subset(DEO9A1_CL, allele_frequency > 0.0499)
 
 
 ###### Applying getSig_QTL function ######
@@ -57,19 +57,19 @@ getSig_QTL(sub_DEO9A1_DD, "sig_DEO9A1_DD.csv")
 getSig_QTL(sub_DEO9A1_CL, "sig_DEO9A1_CL.csv")
 
 #getting numbers of sig SNPs per sig threshold
-table(read.csv("sig_BAP11A1_DD.csv", header = T)[,14])
-table(read.csv("sig_BAP11A1_CL.csv", header = T)[,14])
-table(read.csv("sig_DEO9A1_DD.csv", header = T)[,14])
-table(read.csv("sig_DEO9A1_CL.csv", header = T)[,14])
+table(read.csv("sig_BAP11A1_DD.csv", header = T)[,20])
+table(read.csv("sig_BAP11A1_CL.csv", header = T)[,20])
+table(read.csv("sig_DEO9A1_DD.csv", header = T)[,20])
+table(read.csv("sig_DEO9A1_CL.csv", header = T)[,20])
 
-temp_df <- subset(DEO9A1_CL, p_lrt < 0.0001)#sanity check for last table.  Only obs at this thresh has low allele freq.
+temp_df <- subset(DEO9A1_CL, p_lrt < 0.001)#sanity check for last table.  Only obs at this thresh has low allele freq.
 
 
 #checking on the shared scaffolds between CL and DD
 sig_BAP11A1_DD <- read.csv("sig_BAP11A1_DD.csv", header = T)
 sig_BAP11A1_CL <- read.csv("sig_BAP11A1_CL.csv", header = T)
-sig_DEO9A1_DD <- read.csv("sig_BAP11A1_DD.csv", header = T)
-sig_DEO9A1_CL <- read.csv("sig_BAP11A1_CL.csv", header = T)
+sig_DEO9A1_DD <- read.csv("sig_DEO9A1_DD.csv", header = T)
+sig_DEO9A1_CL <- read.csv("sig_DEO9A1_CL.csv", header = T)
 
 #which scaffolds are shared between DD and CL treatments?
 intersect(unique(sig_BAP11A1_CL$scaffold), unique(sig_BAP11A1_DD$scaffold))
@@ -94,14 +94,18 @@ with_artPos <- data.frame()
     with_artPos <- rbind(with_artPos, comb)
   }
 
-
-For_plot <- data.frame(cbind(with_artPos$snp_id, with_artPos$Chr, with_artPos$radtag_position, with_artPos$p_lrt))
-names(For_plot) <- c("SnpName", "Chr", "Pos", "Pval")
+with_artPos$SnpName <- seq(from = 1, to = nrow(with_artPos), by = 1)
+For_SNPs <- data.frame(cbind(with_artPos$SnpName, with_artPos$Chr, with_artPos$radtag_position, with_artPos$p_lrt, with_artPos$gamma))
+names(For_SNPs) <- c("SnpName", "Chr", "Pos", "Pval", "PIP")
+For_SNPs$SnpName <- as.factor(For_SNPs$SnpName)
+For_plot <- For_SNPs[,-5]
 str(For_plot)#sanity check
 
-SNPs <- subset(For_plot, Pval < sigLev)
-CMplot(For_plot, type="p",plot.type="m", LOG10=TRUE, threshold=NULL,file="jpg",memo=name,dpi=300,
-       col=c("grey50","grey70"), pch = 21, cex = 0.8, ylim = c(0,12), highlight=SNPs$SnpName,
+SNPs <- subset(For_SNPs, Pval < sigLev, PIP > 0.01)
+str(SNPs)
+
+CMplot(For_plot[,-5], type="p",plot.type="m", LOG10=TRUE, threshold=NULL,file="jpg",memo=name,dpi=300,
+       col=c("grey50","grey70"), pch = 21, cex = 0.8, highlight=SNPs[,1],
        highlight.col="dark blue", highlight.cex=0.8, highlight.pch=19, file.output=TRUE,verbose=TRUE,width=14,height=6)
 }
 
