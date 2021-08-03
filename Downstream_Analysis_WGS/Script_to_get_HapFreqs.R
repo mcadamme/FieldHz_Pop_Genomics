038a477278b2725caca85dadf1e8a26b19eeb080
mcadamme <mcadamme@msu.edu>
2021-08-03 15:45:19 -0400

    removed two LGs that no longer met all of our significance criteria

--- Downstream_Analysis_WGS/Script_to_get_HapFreqs.R
+++ Downstream_Analysis_WGS/Script_to_get_HapFreqs.R
@@ -5,6 +5,7 @@ library(vcfR); library(haplo.stats); library(tidyr); library(plyr)
 setwd("/media/fritzlab/EE9C16C89C168AEB/Reanalysis_PNAS")
 
 ##### Chr7 #####
+#this became LG9
 
 #KZ116099.1 - loading datasets
 KZ116099.1_pos <- read.csv("Cry1_KZ116099.1_pos.csv", header = T)
@@ -197,6 +198,7 @@ print(save.em.2017.KZ117108.1, nlines = 12)
 
 
 ###### Chr6 ######
+#this became LG18
 
 #KZ118067.1 - loading datasets
 KZ118067.1_pos <- read.csv("Cry1_KZ118067.1_pos.csv", header = T)
@@ -254,131 +256,8 @@ names(save.em.2017.KZ118067.1)
 print(save.em.2017.KZ118067.1, nlines = 12)
 
 
-
-##### Chr1 #####
-
-#KZ118241.1 - loading datasets
-KZ118241.1_pos <- read.csv("Cry1_KZ118241.1_pos.csv", header = T)
-KZ118241.1 <- read.vcfR("./KZ118241.1_only/KZ118241.1_thinned_FieldHzea_variantsonly.vcf.recode.vcf")
-KZ118241.1_loc <- vcfR2loci(KZ118241.1, return.alleles = TRUE)
-str(KZ118241.1_loc)
-
-#fixing colnames and subsetting by column
-names(KZ118241.1_loc) <- gsub(x = names(KZ118241.1_loc), pattern = "KZ118241.1_", replacement = "")
-
-importLoc <- as.character(names(KZ118241.1_loc))
-
-idx <- match(as.character(KZ118241.1_pos$Pos), importLoc)
-idx <- sort(idx)
-
-New_KZ118241.1_loc <- KZ118241.1_loc[,idx] 
-
-#splitting alleles into separate columns
-KZ118241.1_spl <- data.frame(rownames(New_KZ118241.1_loc))
-names(KZ118241.1_spl) <- "sample"
-
-for (i in 1:ncol(New_KZ118241.1_loc)){
-  vec <- as.vector(New_KZ118241.1_loc[,i])
-  temp <- revalue(vec, c("."="NA/NA"))
-  temp <- data.frame(temp)
-  temp$temp <- as.character(temp$temp)
-  print(head(temp))
-  name1 <- paste0(i,".","1")
-  name2 <- paste0(i,".","2")
-  alleles <- temp %>% separate(col = "temp", into = c(name1,name2), sep = "/")
-  print(head(alleles))
-  KZ118241.1_spl <- cbind(KZ118241.1_spl,alleles)
-}
-
-
-#2002
-KZ118241.1_spl.desc <- summaryGeno(KZ118241.1_spl[1:13,-1], miss.val=c(0,NA))
-save.em.2002.KZ118241.1 <- haplo.em(geno=KZ118241.1_spl[1:13,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2002.KZ118241.1)
-print(save.em.2002.KZ118241.1, nlines = 12)
-
-#2012
-KZ118241.1_spl.desc <- summaryGeno(KZ118241.1_spl[14:24,-1], miss.val=c(0,NA))
-save.em.2012.KZ118241.1 <- haplo.em(geno=KZ118241.1_spl[14:24,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2012.KZ118241.1)
-print(save.em.2012.KZ118241.1, nlines = 12)
-
-#2017
-KZ118241.1_spl.desc <- summaryGeno(KZ118241.1_spl[25:35,-1], miss.val=c(0,NA))
-save.em.2017.KZ118241.1 <- haplo.em(geno=KZ118241.1_spl[24:35,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2017.KZ118241.1)
-print(save.em.2017.KZ118241.1, nlines = 12)
-
-
-##### Chr11 #####
-
-#KZ118133.1 - loading datasets
-KZ118133.1_pos <- read.csv("Cry1_KZ118133.1_pos.csv", header = T)
-max(KZ118133.1_pos$Pos)
-min(KZ118133.1_pos$Pos)
-
-KZ118133.1 <- read.vcfR("./KZ118133.1_only/KZ118133.1_thinned_FieldHzea_variantsonly.vcf.recode.vcf")
-KZ118133.1_loc <- vcfR2loci(KZ118133.1, return.alleles = TRUE)
-str(KZ118133.1_loc)
-
-#fixing colnames and subsetting by column
-names(KZ118133.1_loc) <- gsub(x = names(KZ118133.1_loc), pattern = "KZ118133.1_", replacement = "")
-
-importLoc <- as.character(names(KZ118133.1_loc))
-
-idx <- match(as.character(KZ118133.1_pos$Pos), importLoc)
-idx <- sort(idx)
-
-New_KZ118133.1_loc <- data.frame(KZ118133.1_loc[,idx])
-
-table(New_KZ118133.1_loc[1:13,])#2002 freq
-table(New_KZ118133.1_loc[14:24,])#2012 freq
-table(New_KZ118133.1_loc[25:35,])#2017 freq
-
-#splitting alleles into separate columns
-KZ118133.1_spl <- data.frame(rownames(KZ118133.1_loc))
-names(KZ118133.1_spl) <- "sample"
-
-for (i in 1:ncol(New_KZ118133.1_loc)){
-  vec <- as.vector(New_KZ118133.1_loc[,i])
-  temp <- revalue(vec, c("."="NA/NA"))
-  temp <- data.frame(temp)
-  temp$temp <- as.character(temp$temp)
-  print(head(temp))
-  name1 <- paste0(i,".","1")
-  name2 <- paste0(i,".","2")
-  alleles <- temp %>% separate(col = "temp", into = c(name1,name2), sep = "/")
-  print(head(alleles))
-  KZ118133.1_spl <- cbind(KZ118133.1_spl,alleles)
-}
-
-
-#2002
-KZ118133.1_spl.desc <- summaryGeno(KZ118133.1_spl[1:13,-1], miss.val=c(0,NA))
-save.em.2002.KZ118133.1 <- haplo.em(geno=KZ118133.1_spl[1:13,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2002.KZ118133.1)
-print(save.em.2002.KZ118133.1, nlines = 12)
-
-#2012
-KZ118133.1_spl.desc <- summaryGeno(KZ118133.1_spl[14:24,-1], miss.val=c(0,NA))
-save.em.2012.KZ118133.1 <- haplo.em(geno=KZ118133.1_spl[14:24,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2012.KZ118133.1)
-print(save.em.2012.KZ118133.1, nlines = 12)
-
-#2017
-KZ118133.1_spl.desc <- summaryGeno(KZ118133.1_spl[25:35,-1], miss.val=c(0,NA))
-save.em.2017.KZ118133.1 <- haplo.em(geno=KZ118133.1_spl[24:35,-1], locus.label=idx, miss.val=c(0,NA), 
-                                    control = haplo.em.control(n.try = 20, insert.batch.size=2))
-names(save.em.2017.KZ118133.1)
-print(save.em.2017.KZ118133.1, nlines = 12)
-
-
 ##### Chr5 #####
+#this became LG13
 
 KZ118395.1 <- read.vcfR("./KZ118395.1_only/KZ118395.1_thinned_FieldHzea_variantsonly.vcf.recode.vcf")
 KZ118395.1_loc <- vcfR2loci(KZ118395.1, return.alleles = TRUE)
